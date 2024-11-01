d50ba403076ed6431788aa961b80556b80e3316d
Conner Vieira <cvieira@v0lttech.com>
2024-11-01 14:08:36 -0400

    Improved frame validation

--- utils.py
+++ utils.py
@@ -383,12 +383,12 @@ def get_osd_gps(video, interval=1):
             break
         frame_locations.append(location)
 
-    if (len(frame_locations) > video_frame_count or config["behavior"]["optimization"]["frame_counting"]["skip_validation"] == True): # Check to see if there are more frame locations than frames.
-        if (len(frame_locations) > video_frame_count + video_frame_rate): # Only display an error if the frame location count is off by more than 1 second of video (based on the frame-rate).
+    if (len(frame_locations) > video_frame_count): # Check to see if there are more frame locations than frames.
+        if (len(frame_locations) > video_frame_count + video_frame_rate and config["behavior"]["optimization"]["frame_counting"]["skip_validation"] == False): # Only display an error if the frame location count is off by more than 1 second of video (based on the frame-rate).
             display_message("There were more frame locations (" + str(len(frame_locations)) + ") than frames in the video (" + str(video_frame_count) + ") during GPS OSD analysis.", 2)
         frame_locations = frame_locations[:video_frame_count] # Trim the frame locations down to the length of the video.
     elif (len(frame_locations) < video_frame_count): # Check to see if there are fewer frame locations than frames. This should never happen.
-        if (len(frame_locations) < video_frame_count - video_frame_rate): # Only display an error if the frame location count is off by more than 1 second of video (based on the frame-rate).
+        if (len(frame_locations) < video_frame_count - video_frame_rate and config["behavior"]["optimization"]["frame_counting"]["skip_validation"] == False): # Only display an error if the frame location count is off by more than 1 second of video (based on the frame-rate).
             display_message("There were fewer frame locations (" + str(len(frame_locations)) + ") than frames in the video (" + str(video_frame_count) + ") during GPS OSD analysis.", 2)
 
     while (len(frame_locations) < video_frame_count): # Run in a loop if there are fewer frame locations than frames, and fill them with place-holders.
